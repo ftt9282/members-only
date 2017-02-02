c394ce8c8cc12ff0bcb16a1b3f401ce60e439a46
Frank Torchia <ftt9282@rit.edu>
2017-02-01 22:40:07 -0500

    Add user login plus sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_create :create_remember_token
   has_secure_password
+
+  def User.digest(string)
+    Digest::SHA1.hexdigest string.to_s
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def authenticated?(remember_token)
+    Digest::SHA1.hexdigest(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.new_token
+    self.remember_digest = User.digest(remember_token)
+  end
 end
